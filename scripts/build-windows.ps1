51b3b8e1a99f3a61404d1129dabe59b6f964d93a
Katie McLaughlin <katie@glasnt.com>
2018-08-13 20:43:28 +1000

    Cleanup more

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -1,36 +1,17 @@
 # stop on first error
 $ErrorActionPreference = "Stop"
 
-$ARCH = "Windows"
-$VENV = "venv-" + $ARCH
-
-# cleanup
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue build
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue dist
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue divio_cli.egg-info
-Remove-Item -Recurse -Force -ErrorAction SilentlyContinue $VENV
-
 # create new venv
-
-#$PIP=$(Get-Command pip  | Select-Object -ExpandProperty Definition)
-#& $PIP install virtualenv
-
-#C:\Python27\Scripts\pip.exe list
-#C:\Python27\Scripts\pip.exe install --upgrade pip
-#C:\Python27\Scripts\pip.exe install virtualenv
-#C:\Python27\Scripts\virtualenv.exe $VENV
-
 python -m pip install --upgrade pip
 pip install virtualenv
-virtualenv $VENV
-.\venv-Windows\Scripts\activate
+virtualenv venv
+.\venv\Scripts\activate
 
 # pip and pyinstaller generate lots of warnings, so we need to ignore them
 $ErrorActionPreference = "Continue"
 
 # install build requirements
-
-pip install pyinstaller -r requirements.txt -r requirements-windows.txt -r requirements-build.txt
+pip install  -r requirements.txt -r requirements-windows.txt -r requirements-build.txt
 pip install cryptography
 
 # install divio-cli
@@ -40,9 +21,9 @@ pip install -e .
 md -Force binary
 
 # package app
-.\$VENV\Scripts\pyinstaller.exe -F -y scripts\entrypoint.py -n divio-$ARCH.exe --hidden-import=_cffi_backend --distpath=binary
+pyinstaller.exe -F -y scripts\entrypoint.py -n divio-Windows.exe --hidden-import=_cffi_backend --distpath=binary
 
 $ErrorActionPreference = "Stop"
 
-# run check
-Invoke-Expression ".\binary\divio-$ARCH version"
+# run check against newly built binary
+.\binary\divio-Windows version
